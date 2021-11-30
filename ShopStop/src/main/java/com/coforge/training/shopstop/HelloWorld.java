42af4ba649b0e97354c87c6fbc55c8b4869facb3
kunj verma <kunjkumar3@gmail.com>
2021-11-30 15:19:12 +0530

    Initial Commit

---
+++ ShopStop/src/main/java/com/coforge/training/shopstop/HelloWorld.java
@@ -0,0 +1,50 @@
+package com.coforge.training.shopstop;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	private String msg;
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+        msg="HttpServlet Demo";
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+	
+		//write the message on the web page
+		PrintWriter out=response.getWriter();
+		out.println("<h1>this is first web page</h1>");
+		out.println("<h3>"+msg+"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
